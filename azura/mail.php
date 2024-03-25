71f4c1458ffce34ddd86359706de4c30ae74d7a8
ehas <ehasahamed@gmail.com>
2024-03-25 14:39:57 +0530

    contact form

---
+++ azura/mail.php
@@ -0,0 +1,27 @@
+<?php
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email= $_POST['email'];
+
+$message= $_POST['message'];
+
+
+$subject = "New Enquiry From " . $country . "- wearehazelwood.com";
+$to = "info@wearehazelwood.com, info@wearehazelwood.co.uk";
+$txt = "Full Name : " . $name . "\r\nContact Number : " . $phone . "\r\nSubject : " . $topic . "\r\nMessage : " . $message;
+$subject = " $subject";
+$headers = "From: " . $email;
+
+if (mail($to, $subject, $txt, $headers)) {
+    // If email sent successfully, set JavaScript alert and redirect to index.html
+    echo '<script> window.location.href = "index.html";alert("Message sent successfully!");</script>';
+    exit(); // Make sure to exit after redirecting
+} else {
+    // If email failed to send, return error message
+    $alert_message = "Something went wrong";
+    $alert_type = "failure";
+}
+
+?>
+
